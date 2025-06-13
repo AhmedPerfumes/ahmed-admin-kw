c27d52c80913ce05b1797d2b83f4764da2268238
Yazil Ansari <yazilansari@gmail.com>
2025-06-13 14:31:18 +0400

    Changed Amount Condition

--- platform/plugins/ecommerce/src/Services/CreatePaymentForOrderService.php
+++ platform/plugins/ecommerce/src/Services/CreatePaymentForOrderService.php
@@ -193,7 +193,7 @@ class CreatePaymentForOrderService
                                                                                                                                     <div style="font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;text-align:left;">'.$value->qty.'</div>
                                                                                                                                 </td>
                                                                                                                                 <td style="color:#636363;text-align:left;vertical-align:middle;padding:12px;border:1px solid #E5E5E5;">
-                                                                                                                                    <div style="font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;text-align:left;">&#x062F;&#x002E;&#x0643; 0.00 (Free Gift)'.'</div>
+                                                                                                                                    <div style="font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;text-align:left;">&#x062F;&#x002E;&#x0643; 0.000 (Free Gift)'.'</div>
                                                                                                                                 </td>
                                                                                                                             </tr>';
                                                                                                                         } else {
